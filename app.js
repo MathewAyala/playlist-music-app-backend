bd7568b5ea45a722c420289d2b3cece1c7c992a9
Alvin0820 <your-email@example.com>
2026-07-10 19:21:23 -0400

    New edit

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = 6000;
-const db = require('./db');
-
+const {db} = require('./models');
+const playlistRouter = require('./routes/playlists');
+//const songsRouter = require('./routes/songs');
 
 app.use(express.json())
 
+async function logger(req, res, next){
+    await console.log('Checking request method',req.method, req.originalUrl)
+    next()
+}
+app.use(logger) 
+app.use('/playlists', playlistRouter)
+//app.use('/songs', songsRouter)
+async function startApp(){
+await db.sync
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+}
 
+startApp()
