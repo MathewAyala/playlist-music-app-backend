7cc0bdedd47a93c1f9ac1b585ba6d506526a6504
Alvin0820 <your-email@example.com>
2026-07-11 17:42:05 -0400

    Format new routes

--- routes/playlists.js
+++ routes/playlists.js
@@ -1,42 +1,59 @@
-const router = require('express').Router() 
-const { Playlists } = require("../models")
+const router = require("express").Router();
+const { Playlists, Songs } = require("../models");
 
 router.get("/", async (req, res, next) => {
-    try{
-        const playlist = await Playlists.findAll()
-        res.json(playlist)
-    }catch(err){
-        next(err)
-    }
+  try {
+    const playlist = await Playlists.findAll();
+    res.json(playlist);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get("/:id", async (req, res, next) => {
-    try{
-        const id = Number(req.params.id)
-        const playlist = await Playlists.findByPK(id)
-        res.json(playlist) 
-    }catch(err){
-        next(err)
+  try {
+    const id = Number(req.params.id);
+    const playlist = await Playlists.findByPk(id, { include: Songs });
+    if (!playlist) {
+      return res.status(404).json({ error: "Playlist not found" });
     }
+    res.json(playlist);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-    try{
-        const playlist = await Playlists.create(req.body)
-        res.json(playlist)
-    }catch(err){
-        next(err)
-    }
+  try {
+    const playlist = await Playlists.create(req.body);
+    res.json(playlist);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.delete("/:id", async (req, res, next) => {
-    try{
-        const id = Number(req.params.id)
-        const playlist = await Playlists.findByPK(id)
-        res.json(playlist)
-    }catch(err){
-        next(err)
+  try {
+    const id = Number(req.params.id);
+    const playlist = await Playlists.findByPk(id);
+    if (!playlist) {
+      return res.status(404).json({ error: "Playlist not found" });
     }
+    await playlist.destroy();
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
 });
 
-module.exports = router 
+router.post("/:id/songs", async (req, res, next) => {
+  try {
+    const playlistId = Number(req.params.id);
+    const song = await Songs.create({ ...req.body, playlistId });
+    res.status(201).json(song);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
