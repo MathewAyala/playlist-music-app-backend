6afb5b0ba29bdf403b63f365ac5bf4c4d1d24a1e
MathewAyala <mathewayala05@gmail.com>
2026-07-11 21:07:22 -0400

    changed the port because the previous one was blocked by chrome

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-const PORT = 6000;
+const PORT = 8000; //chrome blocked 6000 -_-
 const { db } = require("./models");
 const playlistRouter = require("./routes/playlists");
 const songsRouter = require("./routes/songs");
+const morgan = require('morgan')
+const cors = require('cors');
+
 
 app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
 async function logger(req, res, next) {
   await console.log("Checking request method", req.method, req.originalUrl);
